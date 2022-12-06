f356a2829e4794b396bf5fc141330c42cce66e96
cardo1011 <ricardomunoz4250@gmail.com>
2022-12-05 23:16:42 -0800

    creating JS-CSS-HTML

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById
+
+startButton.addEventListener('click', startQuiz)
+
+function startQuiz() {
+console.log('Started')
+startButton.classList.add('hide')
+}
+
+function setNextQuestion(){
+
+}
+
+function selectAnswer() {
+
+}
